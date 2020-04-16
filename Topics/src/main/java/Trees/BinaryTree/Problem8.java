8f993915e02c801862488f952780e54e54ca9426
proGeekBoy <63160645+proGeekBoy@users.noreply.github.com>
2020-04-16 11:26:04 +0530

    Amit.K 16/04/2020 Change a Binary Tree so that every node stores sum of all nodes in left subtree

---
+++ Topics/src/main/java/Trees/BinaryTree/Problem8.java
@@ -0,0 +1,58 @@
+package Trees.BinaryTree;
+// Change a Binary Tree so that every node stores sum of all nodes in left subtree
+public class Problem8 {
+    // Function to modify a Binary Tree so that every node
+// stores sum of values in its left child including its
+// own value
+    static int updatetree(Node root)
+    {
+
+        if(null == root)
+        {
+            return 0;
+        }
+        if(root.left == null && root.right == null)
+        {
+            return root.data;
+        }
+        int leftSubTreeSum = updatetree(root.left);
+        int rightSubTreeSum = updatetree(root.right);
+        // add left sub tree to current
+        root.data += leftSubTreeSum;
+        // return root.data+leftsubtree+rightsubtree  (since for parent node rightSubTreeSum is also part of left subtree.)
+        return root.data+rightSubTreeSum;
+    }
+
+    // Utility function to do inorder traversal
+    static void inorder(Node node)
+    {
+        if (node == null)
+            return;
+        inorder(node.left);
+        System.out.print(node.data + " ");
+        inorder(node.right);
+    }
+
+    // Driver program
+    public static void main(String[] args)
+    {
+    /* Let us construct below tree
+                1
+            / \
+            2 3
+            / \ \
+            4 5 6 */
+        Node root = new Node(1);
+        root.left         = new Node(2);
+        root.right     = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+
+        updatetree(root);
+
+
+        System.out.println("Inorder traversal of the modified tree is");
+        inorder(root);
+    }
+}
